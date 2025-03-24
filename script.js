d073a6af4bfc253188bb352de85b5822e630cd4a
vivekkalapet <vivek.ckalapet@gmail.com>
2025-03-24 08:29:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello, world!");
+ const gridItems = document.querySelectorAll('.cell');
+
+
+// Loop through each grid item and add a click event listener
+gridItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // Insert "X" when the div is clicked and it's empty
+        if (item.innerText === '') {
+            item.innerText = 'X';
+}
+
+});
+});
